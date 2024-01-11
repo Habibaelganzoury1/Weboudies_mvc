77b79baf585436dfca6d7d1c4119db5c0d08a6e1
marwanmouradd <marwan.mourad.94@gmail.com>
2024-01-11 02:36:03 +0200

    ...

--- App/views/home.php
+++ App/views/home.php
@@ -1,5 +1,15 @@
 <?php
 //include "../partials/nav.php";
+error_reporting(E_ALL);
+ini_set("display_errors", 0);
+
+function customErrorHandler($errno, $errstr, $errfile, $errline){
+$message = "Error: [[$errno] $errstr - $errfile: $errline]";
+err_log($message, PHP_EOL, 3, "error_log.txt");
+}
+
+set_error_handler("customErrorHandler");
+echo $undefinedVariable
 ?>
 <!DOCTYPE html>
 <html lang="en">
